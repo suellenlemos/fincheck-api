a55bf8d1d36b180781089326a9feb3e36e2ab672
suellen <suellenflickr@gmail.com>
2024-07-12 21:11:02 -0300

    add user modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableShutdownHooks();
+
   await app.listen(3000);
 };
 
